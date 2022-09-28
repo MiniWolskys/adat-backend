3d1580aeea5ed31db9b7b097a4654f9d82586718
Samuel RISON <samuel.rison@sauermanngroup.com>
2022-09-28 14:03:11 +0200

    Starting to split summoner data in bits

--- custom_request/riot_request/summoner_request/summoner_request.py
+++ custom_request/riot_request/summoner_request/summoner_request.py
@@ -1,17 +1,23 @@
 from data_object.summoner_object import SummonerObject
 from custom_request.riot_request import riot_request
 from data_object.factory.custom_object_factory import generate_object_from_riot_api
+from requests import Response
 import requests
 
 url_summoner_by_name = '/lol/summoner/v4/summoners/by-name/'
 
 
-def get_data(region: str, username: str) -> SummonerObject:
+def get_summoner_data(region: str, username: str) -> Response:
     request = riot_request.build_request(region)
     request.url += url_summoner_by_name + username
     ## TODO: Implement custom requesting tool for Riot API
-    res = requests.get(request.url, headers=request.headers)
+    return requests.get(request.url, headers=request.headers)
+
+
+def get_data(region: str, username: str) -> SummonerObject:
+    res = get_summoner_data(region, username)
     if res.status_code != 200:
         # ERROR MANAGEMENT
         pass
-    return generate_object_from_riot_api(res, SummonerObject)
+    summoner_data = generate_object_from_riot_api(res, SummonerObject)
+    return summoner_data
